44e3a17ec75dbfa7f99a11b01dc3106561d8e87a
Michał Przybylak <michal.przybylak@gmail.com>
2018-06-10 19:01:22 +0200

    reading more data from messages

--- src/main/scala/pl/mprzybylak/jackdaw/KafkaReader.scala
+++ src/main/scala/pl/mprzybylak/jackdaw/KafkaReader.scala
@@ -8,30 +8,28 @@ import org.apache.kafka.common.serialization.StringDeserializer
 
 import scala.collection.JavaConversions._
 
-class KafkaReader(topicName: String) {
+class KafkaReader(bootstrapServers:String = "localhost:1234")(topicName: String) {
 
   val props = new Properties()
-  props.put("bootstrap.servers", "localhost:12345")
-  props.put("group.id", "consumer-tutorial")
-
-  import org.apache.kafka.clients.consumer.ConsumerConfig
-
-  props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  props.put("bootstrap.servers", bootstrapServers)
+  props.put("group.id", "jackdaw-client")
   props.put("key.deserializer", classOf[StringDeserializer].getName)
   props.put("value.deserializer", classOf[StringDeserializer].getName)
 
   val consumer = new KafkaConsumer[String, String](props)
 
-  def read(): String = {
+  def read(): Iterable[MessageInfo] = {
     consumer.subscribe(util.Arrays.asList(topicName))
     consumer.poll(1) // hack ftw
-
-
     consumer.seekToBeginning(consumer.assignment())
+
     val msg = consumer.poll(1000)
 
     consumer.close()
-    msg.head.value()
+
+    msg.map(m => MessageInfo(m.key(), m.value(), m.topic(), m.partition(), m.offset(), m.timestamp(), m.timestampType().toString))
   }
 
 }
+
+case class MessageInfo(key: String, value: String, topic: String, partition: Int, offset: Long, timestamp: Long, timestampType: String)
