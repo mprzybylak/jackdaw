50f1b3a1de8af9680a84cca5bbd68c6dfbb49475
Michał Przybylak <michal.przybylak@gmail.com>
2018-06-11 18:04:01 +0200

    WIP

--- src/main/scala/pl/mprzybylak/jackdaw/KafkaReader.scala
+++ src/main/scala/pl/mprzybylak/jackdaw/KafkaReader.scala
@@ -3,33 +3,79 @@ package pl.mprzybylak.jackdaw
 import java.util
 import java.util.Properties
 
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer}
+import org.apache.kafka.common.{PartitionInfo, TopicPartition, TopicPartitionInfo}
 import org.apache.kafka.common.serialization.StringDeserializer
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
 
-class KafkaReader(bootstrapServers:String = "localhost:1234")(topicName: String) {
+
+class KafkaReader(bootstrapServers:String = "localhost:1234")(topicName: String, topics: Seq[String]) {
 
   val props = new Properties()
   props.put("bootstrap.servers", bootstrapServers)
   props.put("group.id", "jackdaw-client")
   props.put("key.deserializer", classOf[StringDeserializer].getName)
   props.put("value.deserializer", classOf[StringDeserializer].getName)
+  props.put("max.poll.record", "1")
 
   val consumer = new KafkaConsumer[String, String](props)
 
-  def read(): Iterable[MessageInfo] = {
-    consumer.subscribe(util.Arrays.asList(topicName))
-    consumer.poll(1) // hack ftw
-    consumer.seekToBeginning(consumer.assignment())
 
-    val msg = consumer.poll(1000)
+  def read(messageToReadPerPartition: Long): Iterable[MessageInfo] = {
+
+
+    // reset position to the end - little bit hacky
+    val t: mutable.Map[String, util.List[PartitionInfo]] = consumer.listTopics().filter(t => t._1 == topicName)
+
+
+    val messages = mutable.HashMap.empty[String, ListBuffer[MessageInfo]]
+
+    for((topicName,partitionInfoes) <- t) {
+      partitionInfoes.foreach {
+        partitionInfo =>
+          consumer.assign(Seq(new TopicPartition(topicName, partitionInfo.partition())))
+          val parititonEnd = consumer.position(new TopicPartition(topicName, partitionInfo.partition()))
+          val paritionStart = partitionStart(messageToReadPerPartition, parititonEnd)
+          for (i <- 0 to paritionStart.asInstanceOf[Number].intValue()) {
+            consumer.seek(new TopicPartition(topicName, partitionInfo.partition()), parititonEnd - i)
+
+            val tMI = toMessageInfo(consumer.poll(1000))
+//            println(tMI)
+
+            tMI match {
+              case Some(m) => {
+                println(m)
+                messages.get(topicName) match {
+                  case Some(msgs) => messages += (topicName -> (msgs += m))
+                  case None => messages += (topicName -> ListBuffer(m))
+                }
+              }
+              case None => {
+                println("Nima ;(")
+              }
+            }
+          }
+      }
+    }
+
+    println(messages)
+
 
     consumer.close()
-
-    msg.map(m => MessageInfo(m.key(), m.value(), m.topic(), m.partition(), m.offset(), m.timestamp(), m.timestampType().toString))
+    List()
   }
 
+  def toMessageInfo(records: ConsumerRecords[String, String]): Option[MessageInfo] = {
+    records.headOption.map(m => MessageInfo(m.key(), m.value(), m.topic(), m.partition(), m.offset(), m.timestamp(), m.timestampType().toString))
+  }
+
+  private def partitionStart(messageToReadPerPartition: Long, parititonEnd: Long) = {
+    if (parititonEnd - messageToReadPerPartition < 0) 0.asInstanceOf[Number].longValue else parititonEnd - messageToReadPerPartition
+  }
 }
 
 case class MessageInfo(key: String, value: String, topic: String, partition: Int, offset: Long, timestamp: Long, timestampType: String)
--- src/test/scala/pl/mprzybylak/jackdaw/KafkaReaderTest.scala
+++ src/test/scala/pl/mprzybylak/jackdaw/KafkaReaderTest.scala
@@ -3,30 +3,42 @@ package pl.mprzybylak.jackdaw
 import java.util.UUID
 
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
-import org.scalatest.WordSpec
-import org.scalatest.Matchers
+import org.apache.kafka.common.serialization.StringSerializer
+import org.scalatest.{Matchers, WordSpec}
 
 class KafkaReaderTest extends WordSpec with EmbeddedKafka with Matchers {
 
   "kafka consumer" should {
+
     "read from topic" in {
 
-      implicit val config = EmbeddedKafkaConfig(kafkaPort = 12345)
+      val prop = Map("unclean.leader.election.enable" -> "true",
+        "min.insync.replicas" -> "1")
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = 1234, customBrokerProperties = prop)
 
       withRunningKafka {
 
         val topicName = "topic"
+        val secondTopic = "secondTopic"
 
-        val expectedMessage = message()
+        createCustomTopic(topicName, Map[String, String](), 3, 1): Unit
+        createCustomTopic("temp", Map[String, String](), 5, 1): Unit
+        implicit val serializer = new StringSerializer
 
-        createCustomTopic(topicName, Map[String,String](), 1, 1): Unit
-        publishStringMessageToKafka(topicName, expectedMessage)
+        for (i <- 1 to 10) {
+          publishToKafka[String, String](topicName, s"key-$i", message())
+          Thread.sleep(2)
+          publishToKafka[String, String](secondTopic, s"secondKey-$i", message())
+          Thread.sleep(10)
 
-        val consumer = new KafkaReader(topicName)
+        }
 
-        consumer.read() shouldEqual expectedMessage
+        val consumer = new KafkaReader()(topicName, Seq(secondTopic))
+
+        println(consumer.read(3).mkString("\n"))
       }
     }
+
   }
 
   private def message() = {
